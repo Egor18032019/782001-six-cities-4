33a16e033a52d1f90953c8b0b2a679a7f2337644
Egor <goro4@mail.ru>
2020-08-02 19:37:24 +0500

    рефакторинг app.jsx.. >? как сделать что бы отрисовка начиналась именно при загрузке данных ??

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,6 +1,6 @@
 import React, {PureComponent} from "react";
 import PropTypes, {bool} from "prop-types";
-import {Switch, Route, Router} from "react-router-dom";
+import {Switch, Route, Router, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import Main from "../Main/main.jsx";
 import withMain from "../hocs/with-main/with-main.js";
@@ -30,72 +30,59 @@ class App extends PureComponent {
 
   // --->?? Максим что тут должно остаться ?
   _renderApp() {
-    const {onMainTitleClick, onCityNameClick, isDataLoaded, activeTown, placesCount, activeOffers, cardId,
-      active, authorizationStatus, onLoginUsers, email, errorMessage, usersErrorMessage} = this.props;
-
-    if (isDataLoaded) {
-      if (authorizationStatus === AuthorizationStatus.AUTH) {
-        if (active === `mainPages`) {
-          return (
-            <MainWrapped
-              placesCount={placesCount}
-              town={activeTown}
-              places={activeOffers}
-              onMainTitleClick={onMainTitleClick}
-              onCityNameClick={onCityNameClick}
-              email={email}
-              authorizationStatus={authorizationStatus}
-            />
-          );
-        } else if (active === `property`) {
-          return (
-            <Property
-              place={activeOffers.find((offer) => {
-                return offer.id === cardId;
-              })}
-            />
-          );
-        }
-      } else {
-        if (usersErrorMessage && authorizationStatus === AuthorizationStatus.AUTH) {
-          // eslint-disable-next-line no-alert
-          alert(`Проверьте логин и пароль`);
-        }
-        // return (
-        //   <SignIn
-        //     onSubmit={onLoginUsers}
-        //   />
-        // );
-      }
-    } else {
+    const {onMainTitleClick, onCityNameClick, activeTown, placesCount, activeOffers, cardId,
+      active, authorizationStatus, email, usersErrorMessage} = this.props;
+    // убрал отображение по статусу загрузки и статусу авторизации
+    if (active === `mainPages`) {
       return (
-        <div className="error" style={{height: `100%`, width: `50%`, paddingTop: `300px`, margin: `auto`, color: `red
-        `}}>
-          <p className="error__message">Ошибка загрузки страницы {errorMessage}</p>
-          <button className="error__button" onClick={()=>{
-            window.location.reload(true);
-          }}>Попробовать снова</button>
-        </div>
+        <MainWrapped
+          placesCount={placesCount}
+          town={activeTown}
+          places={activeOffers}
+          onMainTitleClick={onMainTitleClick}
+          onCityNameClick={onCityNameClick}
+          email={email}
+          authorizationStatus={authorizationStatus}
+        />
       );
+    } else if (active === `property`) {
+      return (
+        <Property
+          place={activeOffers.find((offer) => {
+            return offer.id === cardId;
+          })}
+        />
+      );
+    } else if (usersErrorMessage && authorizationStatus === AuthorizationStatus.NO_AUTH) {
+      // eslint-disable-next-line no-alert
+      alert(`Проверьте введеные даные`);
     }
-    return (
-      <SignIn
-        onLoginUsers={onLoginUsers}
-      />
-    );
+
+    return `что то пошло не так`;
   }
 
   render() {
     const {onMainTitleClick, onCityNameClick, activeTown, placesCount, activeOffers,
-      authorizationStatus, email, onLoginUsers} = this.props;
+      authorizationStatus, email, onLoginUsers, cardId, active, errorMessage} = this.props;
+    let status = (errorMessage ? <div className="error" style={{height: `100%`, width: `50%`, paddingTop: `300px`, margin: `auto`, color: `red`}}>
+      <p className="error__message">Ошибка сети</p>
+      <b>{errorMessage}</b>
+      <br></br>
+      <button className="error__button"
+        onClick={()=>{
+          window.location.reload(true);
+        }}>Попробовать снова</button>
+    </div> : ``);
     return (
       <Router
         history={history}>
         <Switch>
           <Route exact path={AppRoute.ROOT}>
+            {status}
             {this._renderApp()}
           </Route>
           <Route exact path={AppRoute.MAIN}>
+            {status}
             <MainWrapped
               placesCount={placesCount}
               town={activeTown}
@@ -106,26 +93,25 @@ class App extends PureComponent {
               authorizationStatus={authorizationStatus}
             />
           </Route>
-          <Route exact path={AppRoute.PROPERTY}
-            render ={({match})=>{
-              console.log(match);
-              return (
-                <Property
-                  place={activeOffers[0]}
-                  // Макс как быть тут
-                />
-              );
-            }}>
+          <Route exact path={AppRoute.PROPERTY}>
+            {active === `mainPages` ? <Redirect to={AppRoute.MAIN}/> :
+              <Property
+                place={activeOffers.find((offer) => {
+                  return offer.id === cardId;
+                })}
+              />}
+            {status}
           </Route>
           <Route exact path={AppRoute.LOGIN}>
+            {status}
             <SignIn
               onLoginUsers={onLoginUsers}
               activeTown={activeTown}
             />
           </Route>
           <Route exact path={AppRoute.FAVORITES}>
-
           </Route>
+
         </Switch>
       </Router >
     );
